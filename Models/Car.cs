08e93ec6e6d3f3cad9183244e97da292c72fcb82
ZHQ1128 <zhangqian0518@gmail.com>
2018-07-02 09:23:14 +0200

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace CarAssignmentDB.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
